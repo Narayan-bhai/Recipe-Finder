3c292dc8588297f09e554b7144a1c361a1d53d0c
Narayan-bhai <narayan.ratnaparkhe24@pccoepune.org>
2026-02-05 21:13:49 +0530

    added way to downlaod db

---
+++ OnlineDB.py
@@ -0,0 +1,17 @@
+from datasets import load_dataset
+import pandas as pd
+import pickle
+dataset = load_dataset("datahiveai/recipes-with-nutrition")
+
+print(dataset["train"])
+
+df = dataset["train"].to_pandas()
+# create a file callled this or something else
+df.to_pickle("DBCopy.pkl")
+
+# After downloading only below lines are required
+df = pd.read_pickle("DBCopy.pkl")
+
+print(type(df))
+print(df.head())
+
