87a6d7525d3c979f32ae2c4d511ded8a16210ae3
Narayan-bhai <narayan.ratnaparkhe24@pccoepune.org>
2026-02-15 22:40:37 +0530

    recipes using name and amount are recieving

--- back-end/new.py
+++ back-end/new.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from routes.tables import tables_bp
-from flask_cors import CORS
+from routes.tableBluePrint import tables_bp
 from routes.insertInto import *
+from routes.getRecipes import *
+from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(tables_bp)
 
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ df = pd.read_csv("Ingredients_cleaned.csv")
 print(df.columns)
 print(df.dtypes)
 print(df.head())
+print(df["unit"].unique())
 # df["ingredient"] = df["ingredient"].fillna("UNKNOWN").str.strip()
 # unique_ingredients = df["ingredient"].drop_duplicates().reset_index(drop=True)
 # ingredients_df = pd.DataFrame({
